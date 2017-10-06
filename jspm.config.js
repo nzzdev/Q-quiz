d5b39027e42a62996a1a7e4c7f45734611d42877
Beni <beni.buess@nzz.ch>
2017-10-06 18:29:06 +0200

    Revert "fix: add core-js jspm dependency"
    This reverts commit e2e59901f71884265f2f39b7427af0eefbc1a1a4.

--- jspm.config.js
+++ jspm.config.js
@@ -32,11 +32,7 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
-    "core-js": "npm:core-js@2.5.1",
-    "fs": "npm:jspm-nodelibs-fs@0.2.1",
     "leaflet": "github:Leaflet/Leaflet@1.0.3",
-    "path": "npm:jspm-nodelibs-path@0.2.3",
-    "process": "npm:jspm-nodelibs-process@0.2.1",
     "text": "github:systemjs/plugin-text@0.0.9"
   },
   packages: {}
