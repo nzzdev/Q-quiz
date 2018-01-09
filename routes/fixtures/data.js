84b89852e736fd5e9bf6d39ac6362147161ef4e4
Sharon Funke <sharon.funke@nzz.ch>
2018-01-09 16:40:04 +0100

    cleanup buttons and further css classes, adds single quizzes to fixtures

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -3,7 +3,14 @@ const fixtureDataDirectory = '../../resources/fixtures/data';
 // provide every fixture data file present in ../../resources/fixtures/data
 const fixtureData = [
   require(`${fixtureDataDirectory}/all.json`),
-  require(`${fixtureDataDirectory}/cover-all-question-types-no-last-card.json`)
+  require(`${fixtureDataDirectory}/cover-with-title-no-last-card.json`),
+  require(`${fixtureDataDirectory}/single-multiple-choice.json`),
+  require(`${fixtureDataDirectory}/single-number-guess.json`),
+  require(`${fixtureDataDirectory}/single-map-point-guess.json`),
+  // Todo: 
+  // Quiz mit Bildern
+  // Quiz mit Artikelempfehlungen
+  // Quiz mit ausformulierten Antworten
 ];
 
 module.exports = {
