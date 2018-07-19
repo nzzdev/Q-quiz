fe64a251073446f9f8af9445d2be3b4cc2d144e7
Manuel Roth <manuel.roth@nzz.ch>
2018-07-19 15:34:49 +0200

    (mappointguess) Use new map style where roads and bridges are shown

--- script_src/MapPointGuessHandler.js
+++ script_src/MapPointGuessHandler.js
@@ -6,7 +6,7 @@ Leaflet.Icon.Default.imagePath =
   "jspm_packages/github/Leaflet/Leaflet@1.0.3/dist/images";
 
 const tileUrl =
-  "https://api.mapbox.com/styles/v1/neuezuercherzeitung/ciqkqfx6g002rc5m9obtblxlk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmV1ZXp1ZXJjaGVyemVpdHVuZyIsImEiOiJjaXFnbWpvbmMwMDk4aHptY2RiYjM0dHc2In0.Y3HeaE0zhj9OaFEoDIrxRA";
+  "https://api.mapbox.com/styles/v1/neuezuercherzeitung/cj3yj33bk1w5t2rmyy0jty3bb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmV1ZXp1ZXJjaGVyemVpdHVuZyIsImEiOiJjaXFnbWpvbmMwMDk4aHptY2RiYjM0dHc2In0.Y3HeaE0zhj9OaFEoDIrxRA";
 
 const mapOptions = {
   boxZoom: false,
