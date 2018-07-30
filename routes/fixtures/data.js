2686f6d5eb938be64286fdf758df611ce49e1de5
Manuel Roth <manuelroth@hotmail.ch>
2018-07-30 18:02:41 +0200

    Revert "Rename fixture data"
    This reverts commit 622ca5e4c5855aa9476f595888801140c77d786c.

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -3,13 +3,13 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 // provide every fixture data file present in ../../resources/fixtures/data
 const fixtureData = [
   require(`${fixtureDataDirectory}/all.json`),
+  require(`${fixtureDataDirectory}/all-question-types-with-images-and-credit.json`),
   require(`${fixtureDataDirectory}/cover-with-title-no-last-card.json`),
   require(`${fixtureDataDirectory}/single-multiple-choice.json`),
   require(`${fixtureDataDirectory}/single-number-guess.json`),
   require(`${fixtureDataDirectory}/single-map-point-guess.json`),
   require(`${fixtureDataDirectory}/single-map-point-guess-low-zoomlevel.json`),
-  require(`${fixtureDataDirectory}/single-map-point-guess-high-zoomlevel.json`),
-  require(`${fixtureDataDirectory}/with-images-and-credit.json`)
+  require(`${fixtureDataDirectory}/single-map-point-guess-high-zoomlevel.json`)
 ];
 
 module.exports = {
