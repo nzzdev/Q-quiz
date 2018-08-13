8ece3f783b74bb0f1854ac94f63804b0cd6e5021
Manuel Roth <manuelroth@hotmail.ch>
2018-08-13 12:25:25 +0200

    Remove image credit for now

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -3,7 +3,7 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 // provide every fixture data file present in ../../resources/fixtures/data
 const fixtureData = [
   require(`${fixtureDataDirectory}/all.json`),
-  require(`${fixtureDataDirectory}/all-question-types-with-images-and-credit.json`),
+  require(`${fixtureDataDirectory}/all-question-types-with-images.json`),
   require(`${fixtureDataDirectory}/cover-with-title-no-last-card.json`),
   require(`${fixtureDataDirectory}/single-multiple-choice.json`),
   require(`${fixtureDataDirectory}/single-number-guess.json`),
