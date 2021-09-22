ba2570af9befd4aff05b87e3bfaeb37992fbd20d
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-09-22 09:44:08 +0200

    Add fixtures for big and float number guess and poll

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -7,11 +7,15 @@ const fixtureData = [
   require(`${fixtureDataDirectory}/cover-with-title-no-last-card.json`),
   require(`${fixtureDataDirectory}/single-multiple-choice.json`),
   require(`${fixtureDataDirectory}/single-number-guess.json`),
+  require(`${fixtureDataDirectory}/single-big-number-guess.json`),
+  require(`${fixtureDataDirectory}/single-float-number-guess.json`),
   require(`${fixtureDataDirectory}/strip-plot-number-guess.json`),
   require(`${fixtureDataDirectory}/single-map-point-guess.json`),
   require(`${fixtureDataDirectory}/single-map-point-guess-low-zoomlevel.json`),
   require(`${fixtureDataDirectory}/single-map-point-guess-high-zoomlevel.json`),
-  require(`${fixtureDataDirectory}/single-number-poll.json`)
+  require(`${fixtureDataDirectory}/single-number-poll.json`),
+  require(`${fixtureDataDirectory}/single-big-number-poll.json`),
+  require(`${fixtureDataDirectory}/single-float-number-poll.json`)
 ];
 
 module.exports = {
