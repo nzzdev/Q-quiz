d948bcefd059c41cb4bae28926f0b3ea3bf738b7
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-12-09 15:41:58 +0100

    Remove server plugins & update index.js

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const plugins = require("./server-plugins.js");
-const routes = require("./routes/routes.js");
 const Hapi = require("@hapi/hapi");
+const Joi = require("joi");
 
-const start = async function() {
-  let server = Hapi.server({
-    port: process.env.PORT || 3000,
-  });
-  await server.register(plugins);
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  routes: {
+    cors: true,
+  },
+});
+server.validator(Joi);
+
+const routes = require("./routes/routes.js");
+
+async function init() {
+  await server.register(require("@hapi/inert"));
   server.route(routes);
   server.cache.provision({
     provider: {
       constructor: require("@hapi/catbox-memory"),
       options: {
         partition: "x",
-        maxByteSize: 10000
-      }
+        maxByteSize: 10000,
+      },
     },
-    name: "memoryCache"
+    name: "memoryCache",
   });
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-};
+  console.log("server running ", server.info.uri);
+}
 
-start();
+async function gracefullyStop() {
+  console.log("stopping hapi server");
+  try {
+    await server.stop({ timeout: 10000 });
+    console.log("hapi server stopped");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+// listen on SIGINT and SIGTERM signal and gracefully stop the server
+process.on("SIGINT", gracefullyStop);
+process.on("SIGTERM", gracefullyStop);
+
+init();
