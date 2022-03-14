6bef8ff974c65a805e02fd9a3d796c77f21ae730
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-03-14 15:04:07 +0100

    Remove local logging of q-quiz

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const plugins = require("./server-plugins.js");
 const routes = require("./routes/routes.js");
 const Hapi = require("@hapi/hapi");
 
-const start = async function() {
+const start = async function () {
   let server = Hapi.server({
     port: process.env.PORT || 3000,
   });
-  await server.register(plugins);
   server.route(routes);
   server.cache.provision({
     provider: {
       constructor: require("@hapi/catbox-memory"),
       options: {
         partition: "x",
-        maxByteSize: 10000
-      }
+        maxByteSize: 10000,
+      },
     },
-    name: "memoryCache"
+    name: "memoryCache",
   });
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
