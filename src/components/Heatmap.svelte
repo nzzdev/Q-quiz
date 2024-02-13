3795fbe0f83caabcf1c77f4ba8f5f59219c8811f
Franco Gervasi <franco.gervasi@nzz.ch>
2024-02-13 07:35:17 +0100

    Fix svelte error

--- src/components/Heatmap.svelte
+++ src/components/Heatmap.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import L from 'leaflet';
+
+  import type { MapPointGuessStatistic } from '@src/interfaces';
+
   import '../services/leafle-heatmap.cjs';
   import simpleheat from '../services/simpleheat.cjs';
-  import type { MapPointGuessStatistic } from '@src/interfaces';
 
   export let data: MapPointGuessStatistic[];
   export let map: L.Map;
 
   onMount(() => {
-    // taken from https://github.com/pa7/heatmap.js (MIT-licensed)
-
-    //@ts-ignore
     L.heatLayer(
       simpleheat,
       data.map((p) => {
