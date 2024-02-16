068983ee7ec68b43d9454b1a10f936ff58ce86ef
Franco Gervasi <franco.gervasi@nzz.ch>
2024-02-16 14:14:29 +0100

    Implement header

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Divider from './atomic/Divider.svelte';
+  export let title: string;
+</script>
+
+<div class="layout-header q-quiz-gap-column">
+  <div class="s-font-note-s">{title}</div>
+  <Divider />
+</div>
+
+<style lang="scss">
+  .layout-header {
+    text-align: center;
+    padding-top: 5px;
+  }
+</style>
--- src/components/index.svelte
+++ src/components/index.svelte
@@ -12,11 +12,13 @@
   } from '@src/interfaces';
   import { QuizElementType } from '@src/enums';
 
+  import Header from './Header.svelte';
   import CoverComponent from './cover/Cover.svelte';
   import LastCardComponent from './last-card/LastCard.svelte';
   import MutliplieChoice from './input-multiple-choice/MultipleChoice.svelte';
   import NumberSlider from './number-slider/NumberSlider.svelte';
   import InputMapPoint from './input-map-point-guess/InputMapPoint.svelte';
+  import Cover from './cover/Cover.svelte';
 
   export let componentConfiguration: QQuizSvelteProperties;
 
@@ -35,6 +37,13 @@
 
   $: containerWidthStore.set(containerWidth);
   $: indexAdditional = $quizStore.hasCover ? 0 : 1;
+  $: title = () => {
+    const cover = $quizStore.items.find(
+      (item) => item.item.type === QuizElementType.Cover
+    )?.item as Cover | undefined;
+
+    return cover?.title;
+  };
 
   onMount(() => {
     quizStore.initialize(componentConfiguration);
@@ -52,28 +61,32 @@
       {#if $quizStore.step === index + indexAdditional}
         {#if element.item.type === QuizElementType.Cover}
           <CoverComponent element={element.item} />
+        {:else}
+          <div
+            class="element-card"
+            in:fly={{ x: '100%', delay: 380 }}
+            out:fly={{ x: '-100%' }}
+          >
+            {#if $quizStore.hasCover && $quizStore.isMultiQuiz}
+              <Header title={title()} />
+            {/if}
+            {#if element.item.type === QuizElementType.LastCard}
+              <LastCardComponent element={element.item} />
+            {:else if element.item.type === QuizElementType.MultipleChoice}
+              <MutliplieChoice
+                element={element.item}
+                toolBaseUrl={$quizStore.configuration.toolBaseUrl}
+              />
+            {:else if element.item.type === QuizElementType.NumberGuess || element.item.type === QuizElementType.NumberPoll}
+              <NumberSlider
+                element={element.item}
+                toolBaseUrl={$quizStore.configuration.toolBaseUrl}
+              />
+            {:else if element.item.type === QuizElementType.MapPointGuess}
+              <InputMapPoint element={element.item} />
+            {/if}
+          </div>
         {/if}
-        <div
-          class="element-card"
-          in:fly={{ x: '100%', delay: 380 }}
-          out:fly={{ x: '-100%' }}
-        >
-          {#if element.item.type === QuizElementType.LastCard}
-            <LastCardComponent element={element.item} />
-          {:else if element.item.type === QuizElementType.MultipleChoice}
-            <MutliplieChoice
-              element={element.item}
-              toolBaseUrl={$quizStore.configuration.toolBaseUrl}
-            />
-          {:else if element.item.type === QuizElementType.NumberGuess || element.item.type === QuizElementType.NumberPoll}
-            <NumberSlider
-              element={element.item}
-              toolBaseUrl={$quizStore.configuration.toolBaseUrl}
-            />
-          {:else if element.item.type === QuizElementType.MapPointGuess}
-            <InputMapPoint element={element.item} />
-          {/if}
-        </div>
       {/if}
     {/each}
   {/if}
